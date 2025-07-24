654bfe14d24335d70098910e9cc37e7d574cf637
Iskandarius <133978269+Iskandarius@users.noreply.github.com>
2025-07-24 18:22:10 +0500

    Update test_auth.py

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -13,38 +13,38 @@ def test_auth_chrome(driver_chrome):
     auth_page.click_btn_next()
     auth_page.enter_password("proweb3106632")
     auth_page.click_btn_submit()
-    # try:
-    #     auth_page.click_btn_sessions()
-    #     auth_page.click_btn_finish()
-    # except:
-    #     pass
+    try:
+        auth_page.click_btn_sessions()
+        auth_page.click_btn_finish()
+    except:
+        pass
     time.sleep(3)
     auth_page.click_coworking()
 
-    # home_page = HomePage(driver_chrome)
-    # home_page.click_profile_icon()
-    # time.sleep(5)
-    # home_page.click_exit_btn()
-    # home_page.click_confirm_exit()
-
-
-    coworking_page = CoworkingPage(driver_chrome)
-    coworking_page.click_sign_up_btn()
-    coworking_page.click_aybek_2_btn()
-    coworking_page.click_select_btn_1()
-    coworking_page.click_date_btn()
-    coworking_page.click_group_btn()
-    coworking_page.click_round_btn()
-    coworking_page.click_select_btn_2()
-    coworking_page.enter_time()
-    coworking_page.enter_hours("11")
-    coworking_page.enter_minutes("00")
+    home_page = HomePage(driver_chrome)
+    home_page.click_profile_icon()
     time.sleep(5)
-    coworking_page.click_select_btn_3()
-    coworking_page.click_place()
-    coworking_page.click_computer()
-    coworking_page.click_select_btn_4()
-    coworking_page.click_send_btn()
+    home_page.click_exit_btn()
+    home_page.click_confirm_exit()
+
+
+    # coworking_page = CoworkingPage(driver_chrome)
+    # coworking_page.click_sign_up_btn()
+    # coworking_page.click_aybek_2_btn()
+    # coworking_page.click_select_btn_1()
+    # coworking_page.click_date_btn()
+    # coworking_page.click_group_btn()
+    # coworking_page.click_round_btn()
+    # coworking_page.click_select_btn_2()
+    # coworking_page.enter_time()
+    # coworking_page.enter_hours("11")
+    # coworking_page.enter_minutes("00")
+    # time.sleep(5)
+    # coworking_page.click_select_btn_3()
+    # coworking_page.click_place()
+    # coworking_page.click_computer()
+    # coworking_page.click_select_btn_4()
+    # coworking_page.click_send_btn()
 
 
 def test_invalid_auth_chrome(driver_chrome):
